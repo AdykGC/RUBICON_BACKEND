e3a3f262c3ede6c08b0a29413e0681a516e86581
Altynbek Adilkhan <Adyk2004gc@gmail.com>
2026-04-23 06:21:37 +0500

    Deploy Setup 35

--- Backend/database/factories/MachineFactory.php
+++ Backend/database/factories/MachineFactory.php
@@ -1,10 +1,13 @@
 <?php namespace Database\Factories;
 
 use App\Models\User;
+use App\Models\Machine;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class MachineFactory extends Factory
 {
+    protected $model = Machine::class;
+
     public function definition(): array
     {
         return [
@@ -12,7 +15,7 @@ class MachineFactory extends Factory
             'type' => $this->faker->randomElement(['Snack', 'Drink', 'Coffee', 'Combo']),
             'location' => $this->faker->address(),
             'serial_number' => $this->faker->unique()->bothify('RUB-######'),
-            'user_id' => User::factory(), // автоматически создаст пользователя
+            'user_id' => User::factory(),
             'connection_type' => $this->faker->randomElement(['4G', 'WiFi', 'Ethernet']),
             'install_price' => $this->faker->randomFloat(2, 5000, 20000),
             'price_adjustment' => $this->faker->randomFloat(2, 0, 500),
--- Backend/database/factories/UserFactory.php
+++ Backend/database/factories/UserFactory.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace Database\Factories;
+<?php namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -8,14 +6,10 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends Factory<User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
     protected $model = User::class;
 
     /**
@@ -26,21 +20,24 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'name' => $this->faker->name(),
+            'company_title' => $this->faker->company(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'password' => Hash::make('password'),
+            'is_active' => true,
             'remember_token' => Str::random(10),
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Indicate that the user is inactive.
      */
-    public function unverified(): static
+    public function inactive(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+            'is_active' => false,
         ]);
     }
 }
