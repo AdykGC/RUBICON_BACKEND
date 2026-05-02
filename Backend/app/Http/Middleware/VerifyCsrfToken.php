0f9b832c888c3d73bbfbae9cad3a915bae8810f4
Altynbek Adilkhan <Adyk2004gc@gmail.com>
2026-05-03 02:44:25 +0500

    Deploy Setup 74

--- Backend/app/Http/Middleware/VerifyCsrfToken.php
+++ Backend/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,12 +12,14 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/bitrix/install',
-        '/bitrix/uninstall',
-        '/bitrix/dashboard',
-        '/bitrix/placement/*',
-        '/bitrix/events/*',
-        '/bitrix/install/ui',
-        '/bitrix/install/ui-plain',
+        'bitrix/install/*', // Укажите здесь ваш маршрут для обработки установки/инициализации
+        'bitrix/install/ui-lite',
+        'bitrix/uninstall',
+        //'/bitrix/dashboard',
+        'bitrix/dashboard', // Или маршрут, который вызывает ошибку при входе
+        'bitrix/placement/*',
+        'bitrix/events/*',
+        'bitrix/install/ui',
+        'bitrix/install/ui-plain',
     ];
 }
