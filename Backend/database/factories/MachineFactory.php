d583426d7b4e689f92da2d85ca1a2b40acbe92c2
Altynbek Adilkhan <Adyk2004gc@gmail.com>
2026-05-09 16:50:06 +0500

    Deploy Setup 102

--- Backend/database/factories/MachineFactory.php
+++ Backend/database/factories/MachineFactory.php
@@ -2,41 +2,34 @@
 
 namespace Database\Factories;
 
+use App\Models\Machine;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class MachineFactory extends Factory
 {
+    protected $model = Machine::class;
+
     public function definition(): array
     {
         return [
             'name' => 'Автомат '.$this->faker->randomNumber(3),
-
             'type' => $this->faker->randomElement([
                 'Drink',
                 'Snack',
                 'Coffee',
             ]),
-
             'location' => $this->faker->address(),
-
             'mac_address' => $this->faker->macAddress(),
-
             'connection_type' => $this->faker->randomElement([
                 '4G',
                 'WiFi',
                 'Ethernet',
             ]),
-
             'install_price' => $this->faker->randomFloat(2, 10000, 100000),
-
             'price_adjustment' => $this->faker->randomFloat(2, -10, 10),
-
             'latitude' => $this->faker->latitude(),
-
             'longitude' => $this->faker->longitude(),
-
             'balance' => $this->faker->randomFloat(2, 0, 10000),
-
             'is_active' => true,
         ];
     }
